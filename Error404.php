419bccadc4c25a21ad0f626ca2d0ada6a18ac188
Artur Gabriel <ga29092015@hotmail.com>
2022-11-24 23:58:16 -0400

    Commit

---
+++ Error404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Error 404</title>
+</head>
+<body>
+    <div class="p-5 text-center ">
+        <div>
+            <img src="Images/roboquebradop.png" style="width: 200px; margin-top:80px; margin-bottom:30px;"/>
+        </div>
+        <div style="margin-bottom:50px;">
+
+            <h1 style="margin-left: 51px;">Error 404</h1>
+            <h6 style="margin-left: 56px;">Não encontramos a página solicitada</h6>
+        </div>
+    </div>
+    <?php include '_parts/_linkJS.php'; ?>
+</body>
+
+</html>
